d6db9dbff799c16b07e7ac5a5b475c7850fe8eb2
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2024-04-11 20:44:12 +0100

    Add a go.sum to enable GitHub caching
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/actuated-samples/checkout-go
 
 go 1.20
+
+require golang.org/x/crypto v0.22.0
--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "golang.org/x/crypto/bcrypt"
 
+func main() {
+	data, err := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+
+	println(string(data))
 }
