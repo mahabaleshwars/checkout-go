6b78cf573dd92ba32d0cce657e5e21a6ced3215e
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2024-04-11 20:45:45 +0100

    Some random data
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"math/rand"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 func main() {
-	data, err := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.DefaultCost)
+
+	r := rand.NewSource(time.Now().UnixMilli())
+	chars := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	var passwd string
+	for i := 0; i < 8; i++ {
+		passwd += string(chars[r.Int63()%int64(len(chars))])
+	}
+
+	data, err := bcrypt.GenerateFromPassword([]byte(passwd), bcrypt.DefaultCost)
 	if err != nil {
 		panic(err)
 	}
